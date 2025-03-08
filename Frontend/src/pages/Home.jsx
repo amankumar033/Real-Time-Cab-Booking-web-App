4143ab7e65c68ffc550d3b05b0fa1926bf1e7a8e
amankumar033 <166852922+amankumar033@users.noreply.github.com>
2025-03-08 12:40:47 +0530

    userlogin

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <div className='bg-cover bg-[url(https://plus.unsplash.com/premium_photo-1731842686156-74895c29a87b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8dHJhZmZpYyUyMGxpZ2h0c3xlbnwwfHwwfHx8MA%3D%3D)]  h-screen w-full flex flex-col justify-between '>
+
+      
+        <div>
+          <img className='w-16 absolute left-5 top-5' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+        </div>
+
+        
+        <div className='bg-white  p-4'>
+          <h2 className='text-2xl font-bold '>Get Started With Uber</h2>
+          <Link to="/userLogin" className='flex items-center justify-center bg-black text-white  py-2 mt-4 mb-3 w-full rounded-lg'>Continue</Link>
+        </div>
+
+      </div>
+    </>
   )
 }
 
--- Frontend/src/pages/userLogin.jsx
+++ Frontend/src/pages/userLogin.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const UserLogin = () => {
   return (
-    <div>UserLogin</div>
+    <>
+    <div className='p-5'>
+      
+
+    <img className='w-16 mb-8' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+      
+     <form className='flex flex-col  '>
+      <h3 className='font-semibold text-lg mb-1' >What's your email</h3>
+      <input className='bg-[#eeeeee] py-2 px-2 text-sm placeholder:text-base  rounded mb-4' type="email"  placeholder='email@example.com' />
+      <h3 className='font-semibold text-lg mb-1' >Enter Password</h3>
+      <input className='bg-[#eeeeee] py-2 px-2 text-sm placeholder:text-base mb-6' type="password" placeholder='password'  />
+      <button className='bg-black text-white rounded py-2'>Login</button>
+     </form>
+   
+    </div>
+    </>
   )
 }
 
