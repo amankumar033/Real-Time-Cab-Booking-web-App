c478ed6fcf8df27e948d382af923d0cc2355300d
amankumar033 <166852922+amankumar033@users.noreply.github.com>
2025-03-08 19:14:27 +0530

    userlogin

--- Frontend/src/pages/userLogin.jsx
+++ Frontend/src/pages/userLogin.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const UserLogin = () => {
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+  const [userData,setuserData] = useState({});
+  const submitHandler=(e)=>{
+    e.preventDefault();
+    console.log("user email:",email)
+    console.log("user password:",password)
+     setuserData({
+      email:email,
+      password:password
+     })
+     console.log("userdata is :",userData);
+    setEmail('');
+    setPassword('');
+  }
   return (
     <>
     <div className='p-5  h-screen flex flex-col justify-between'>
@@ -11,17 +26,17 @@ const UserLogin = () => {
 
     <img className='w-16 mb-8' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
       
-     <form className='flex flex-col  '>
+     <form onSubmit={submitHandler} className='flex flex-col  '>
       <h3 className='font-semibold text-lg mb-1' >What's your email</h3>
-      <input required className='bg-[#eeeeee] py-2 px-2 text-sm placeholder:text-base  rounded mb-5' type="email"  placeholder='email@example.com' />
+      <input value={email} onChange={(e)=>{setEmail(e.target.value)}} required className='bg-[#eeeeee] py-2 px-2 text-sm placeholder:text-base  rounded mb-5' type="email"  placeholder='email@example.com' />
       <h3 className='font-semibold text-lg mb-1' >Enter Password</h3>
-      <input required className='bg-[#eeeeee] py-2 px-2 text-sm placeholder:text-base mb-9' type="password" placeholder='password'  />
+      <input  value={password} onChange={(e)=>{setPassword(e.target.value)}} required className='bg-[#eeeeee] py-2 px-2 text-sm placeholder:text-base mb-9' type="password" placeholder='password'  />
       <button className='bg-black text-white rounded py-2 mb-3 font-semibold'>Login</button>
      </form>
      <p className='text-center' >New here? <Link to={'/usersignup'} className='text-blue-600'>Create New Account</Link></p>
      </div>
     <div>
-      <button className='bg-green-400 w-full rounded py-2 font-semibold text-white mb-10'>Sign in as Captain</button>
+      <Link to={'/captainlogin'} className='flex justify-center bg-green-400 w-full rounded py-2 font-semibold text-white mb-10'>Sign in as Captain</Link>
     </div>
     </div>
     </>
