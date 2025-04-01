71987cdb0fa9148ee2d7bdff764014b409dde4a7
amankumar033 <166852922+amankumar033@users.noreply.github.com>
2025-04-01 11:02:21 +0530

    updated

--- Frontend/src/pages/UserHome.jsx
+++ Frontend/src/pages/UserHome.jsx
@@ -2,23 +2,25 @@ import React from 'react'
 
 const UserHome = () => {
   return (
-    <div>
+    <div className='relative h-screen  '>
       <img className='w-16 absolute mb-8 left-5  top-5' src="/assets/Uber_logo.png" alt="" />
       <div className='h-screen w-screen '>
         {/* image for temporary use */}
         <img  className='h-full w-full ' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
       </div>
-      <div className='bg-white border-2 rounded-tl-xl rounded-tr-xl p-3 '>
-        
+      <div className='bg-white rounded-tl-2xl rounded-tr-xl p-3 absolute w-full top-0 h-screen'>  
+        <div className='h-[40%]'>
        <h1 className='text-xl font-semibold mb-3'>Find a trip</h1>
-       
-       <form className='flex flex-col gap-3' action="">
-        <input type="text"   placeholder='Add a pick-up location'/>
-        <input type="text"  placeholder='Enter your destination'/>
-        <button className='border'>Leave Now</button>
-        
+       <form action="">
+        <input type="text" className='bg-[#eee] p-3 rounded-2xl text-base mt-3 w-full'   placeholder='Add a pick-up location'/>
+        <input type="text" className='bg-[#eee] p-3 rounded-xl   text-base mt-5 w-full'  placeholder='Enter your destination'/>
+        <button className='border p-2 w-max mt-5'>Leave Now</button>
        </form>
-   
+   </div>
+      
+      <div className='bg-red-500 w-full h-[60%]'>
+
+      </div>
       </div>
 
     </div>
