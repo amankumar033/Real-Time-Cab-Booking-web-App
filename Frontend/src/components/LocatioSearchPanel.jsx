7a93e987cca054a0243d3bbe47599d92e3b6d1f9
amankumar033 <166852922+amankumar033@users.noreply.github.com>
2025-04-01 15:20:19 +0530

    updated

---
+++ Frontend/src/components/LocatioSearchPanel.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LocatioSearchPanel = () => {
+  return (
+    <div>LocatioSearchPanel</div>
+  )
+}
+
+export default LocatioSearchPanel
--- Frontend/src/pages/UserHome.jsx
+++ Frontend/src/pages/UserHome.jsx
@@ -2,11 +2,14 @@ import React, { useRef } from "react";
 import { useState } from "react";
 import {useGSAP} from "@gsap/react"
 import { gsap } from "gsap";
+import 'remixicon/fonts/remixicon.css'
+import LocatioSearchPanel from "../components/LocatioSearchPanel";
 const UserHome = () => {
   const [pickUpLocation, setPickUpLocation] = useState("");
   const [destination, setDestination] = useState("");
   const [panelOpen, setPanelOpen] = useState(false);
   const panelRef=useRef(null)
+  const panelClose =useRef(null)
   const submitHandler = (e) => {
     e.preventDefault();
     console.log("form submitted Data:", { pickUpLocation, destination });
@@ -17,13 +20,19 @@ const UserHome = () => {
     if(panelOpen){
       gsap.to(panelRef.current, {
         height:"70%",
-        duration:0.8,
-    })  
+        duration:0.8,})
+        gsap.to(panelClose.current, {
+          opacity:1
+        })
+    
     }
     else{
       gsap.to(panelRef.current, {
         height:"0%"
-    })  
+    }) 
+    gsap.to(panelClose.current, {
+      opacity:0
+    }) 
     }
 },[panelOpen])
   return (
@@ -46,6 +55,8 @@ const UserHome = () => {
         <div className="h-[40%] rounded-tl-2xl rounded-tr-xl p-5 bg-white relative">
           <h1 className="text-xl font-semibold mb-3">Find a trip</h1>
 
+          <img onClick={() => setPanelOpen(false)} ref={panelClose} className="absolute top-6 right-6" src="/assets/arrow-down-s-line.png" alt="" />
+
           <form onSubmit={(e) => submitHandler(e)} className="flex flex-col">
             <input
               type="text"
@@ -57,7 +68,7 @@ const UserHome = () => {
               onClick={() => {
                 setPanelOpen(true);
               }}
-              className="bg-[#eee] py-3 px-12 rounded-2xl text-base mt-3 w-full"
+              className="bg-[#eee] py-3 px-12 rounded-xl text-base mt-3 w-full"
               placeholder="Add a pick-up location"
             />
             <div className="line w-1 h-17 left-11 top-24 absolute rounded bg-gray-700"></div>
@@ -71,15 +82,17 @@ const UserHome = () => {
               onClick={() => {
                 setPanelOpen(true);
               }}
-              className="bg-[#eee] p-3 rounded-xl text-base mt-5 w-full py-3 px-12"
+              className="bg-[#eee]  rounded-xl text-base mt-4 w-full py-3 px-12"
               placeholder="Enter your destination"
             />
-            <button className="border p-2 w-max mt-5 rounded-2xl">
+            <button className="border p-2 w-max mt-5 rounded-xl">
               Leave Now
             </button>
           </form>
         </div>
-        <div ref={panelRef} className="bg-red-500 w-full   "></div>
+        <div ref={panelRef} className="bg-red-500 w-full ">
+          <LocatioSearchPanel/>
+        </div>
       </div>
     </div>
   );
