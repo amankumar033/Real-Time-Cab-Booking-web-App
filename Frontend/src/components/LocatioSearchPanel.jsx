2c1b0b5a3430e3618af8848ffb6eeb020ae534a0
amankumar033 <166852922+amankumar033@users.noreply.github.com>
2025-04-02 18:29:07 +0530

    updated

--- Frontend/src/components/LocatioSearchPanel.jsx
+++ Frontend/src/components/LocatioSearchPanel.jsx
@@ -7,12 +7,14 @@ const LocationSearchPanel = () => {
         "D-3/1, Junior Wing, Sector-32, Noida, Uttar Pradesh, India-23301",
         "E-2/12, Army Kant, Sector-6, Noida, Uttar Pradesh, India-25301",
         "F-11/0, Nally, Sector-22, Noida, Uttar Pradesh, India-201331",
+        "G-11/0, Brally, Sector-9, Noida, Uttar Pradesh, India-222331",
+      
     ];
 
     return (
-        <div className='flex flex-col gap-5 rounded-xl mr-5'>
+        <div className='flex flex-col gap-2 rounded-xl mr-5 mb-10'>
             {locations.map((location, index) => (
-                <div key={index} className='flex gap-2 items-center p-2  rounded-xl active:border-2'>
+                <div key={index} className='flex gap-2 items-center p-2  rounded-xl active:border-2 '>
                     <img className='w-8 h-7 rounded-lg' src='/assets/map-drop-pin-icon.jpg' alt='Location Pin' />
                     <p>{location}</p>
                 </div>
--- Frontend/src/pages/UserHome.jsx
+++ Frontend/src/pages/UserHome.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef } from "react";
 import { useState } from "react";
-import {useGSAP} from "@gsap/react"
+import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
-import 'remixicon/fonts/remixicon.css'
+import "remixicon/fonts/remixicon.css";
 import LocatioSearchPanel from "../components/LocatioSearchPanel";
 const UserHome = () => {
   const [pickUpLocation, setPickUpLocation] = useState("");
@@ -10,70 +10,146 @@ const UserHome = () => {
   const [panelOpen, setPanelOpen] = useState(false);
   const [vehiclePanelOpen, setVehiclePanelOpen] = useState(false);
   const [mapOpen, setMapOpen] = useState(false);
-  const panelRef=useRef(null)
-  const arrowPanelClose =useRef(null)
-  const vehiclePanelRef=useRef(null)
-  const mapOpacity= useRef(null)
-  const formClose=useRef(null)
+  const panelRef = useRef(null);
+  const arrowPanelClose = useRef(null);
+  const vehiclePanelRef = useRef(null);
+  const mapOpacity = useRef(null);
+  const locationPanelClose = useRef(null);
+  const formClose = useRef(null);
+  const uberLogo = useRef(null);
   const submitHandler = (e) => {
     e.preventDefault();
-    if(panelOpen){
-    setVehiclePanelOpen(true);}
+      setVehiclePanelOpen(true);
+  
     console.log("form submitted Data:", { pickUpLocation, destination });
     setPickUpLocation("");
     setDestination("");
   };
-  useGSAP(function(){
-    if(panelOpen){
-      gsap.to(panelRef.current, {     
-        height:"60%",
-       visibility:"visible",
-        paddingLeft:24,
-        duration:0.8,})
-     gsap.to(arrowPanelClose.current, {
-          opacity:1
-        })
-     gsap.to(mapOpacity.current, {
-          opacity:0,
-          duration:2,
-        })
-    
-    }
-    else{
-      gsap.to(panelRef.current, {
-        height:"0%",
-        padding:0,
-        duration:0.8,
-         }) 
-      gsap.to(arrowPanelClose.current, {
-      opacity:0
-    }) 
-    gsap.to(mapOpacity.current, {
-      opacity:1,
-      duration:1.5,
-    })
-    }
-},[panelOpen])
+  useGSAP(
+    function () {
+      if (panelOpen) {
+        gsap.to(panelRef.current, {
+          height: "60%",
+          opacity: 1,
+          paddingLeft: "24",
+          duration: 1,
+          ease: "power2.out",
+          display: "block",
+          visibility: "visible",
+        });
+        gsap.to(locationPanelClose.current, {
+          height: "100%",
+          duration: 0.8,
+        });
 
-  useGSAP(function(){
-    if(vehiclePanelOpen){
-      gsap.to(vehiclePanelRef.current, {
-        transform:"translateY(0%)",        
-        duration:0.8,
-      })
-      gsap.to(panelRef.current, {
-       visibility:"hidden",
-        duration:0.8,
-      })
-    }
-    else{
-      gsap.to(vehiclePanelRef.current, {
-        transform:"translateY(100%)",
-        duration:0.8,
-      })
-    }
-  },[vehiclePanelOpen])
- 
+        gsap.to(arrowPanelClose.current, {
+          opacity: 1,
+        });
+        gsap.to(mapOpacity.current, {
+          opacity: 0,
+          duration: 0.8,
+          pointerEvents: "none",
+        });
+        gsap.to(uberLogo.current, {
+          opacity: 0,
+          duration: 0.8,
+        });
+      } else {
+        gsap.to(panelRef.current, {
+          padding: 0,
+          opacity: 0,
+          height: 0,
+          marginTop: 0,
+          duration: 0.9,
+          onComplete: () => {
+            panelRef.current.style.display = "none";
+          },
+        });
+        gsap.to(locationPanelClose.current, {
+          height: "auto",
+          duration: 0.8,
+        });
+        gsap.to(arrowPanelClose.current, {
+          opacity: 0,
+        });
+        gsap.to(mapOpacity.current, {
+          opacity: 1,
+          duration: 1.5,
+          pointerEvents: "auto",
+        });
+
+        gsap.to(uberLogo.current, {
+          opacity: 1,
+          duration: 0.8,
+        });
+      }
+    },
+    [panelOpen]
+  );
+
+  useGSAP(
+    function () {
+      if (vehiclePanelOpen) {
+        gsap.to(vehiclePanelRef.current, {
+          transform: "translateY(0%)",
+          duration: 0.8,
+        });
+        gsap.to(panelRef.current, {
+          height: "60%",
+          opacity: 0,
+          paddingLeft: "24",
+          duration: 1,
+          ease: "power2.out",
+          display: "block",
+          visibility: "visible",
+        })
+        
+        gsap.to(locationPanelClose.current, {
+          height: "100%",
+          duration: 0.8,
+        });
+        gsap.to(arrowPanelClose.current, {
+          opacity: 1,
+        });
+        gsap.to(mapOpacity.current, {
+          opacity: 0,
+          duration: 0,
+          pointerEvents: "none",
+        });
+      } else {
+        gsap.to(vehiclePanelRef.current, {
+          transform: "translateY(100%)",
+          duration: 0.9,
+        });
+        gsap.to(locationPanelClose.current, {
+          height: "auto",
+          duration: 0.8,
+        });
+        gsap.to(arrowPanelClose.current, {
+          opacity: 0,
+        });
+        gsap.to(mapOpacity.current, {
+          opacity: 1,
+          duration: 1.5,
+          pointerEvents: "auto",
+        });
+
+        gsap.to(uberLogo.current, {
+          opacity: 1,
+          duration: 0.8,
+        });
+        if (panelOpen) {
+          gsap.to(panelRef.current, {
+            visibility: "visible",
+            duration: 0.8,
+          });
+
+        }
+      }
+    },
+    [vehiclePanelOpen]
+  );
+
   useGSAP(() => {
     if (mapOpen) {
       gsap.to(formClose.current, {
@@ -89,34 +165,60 @@ const UserHome = () => {
       });
     }
   }, [mapOpen]);
-  
-  
+
   return (
     <div className="h-screen overflow-hidden relative">
-      <img 
+      <img
+        ref={uberLogo}
         className="w-16 absolute mb-8 left-5  top-5"
         src="/assets/uber_logo.png"
         alt=""
       />
-      <div  ref={mapOpacity} className="h-screen w-screen ">
+
+      <div
+        ref={mapOpacity}
+        onClick={() => {
+          setMapOpen(true);
+        }}
+        className="h-screen w-screen "
+      >
         {/* image for temporary use */}
-        <img 
-  className="h-full w-full object-cover"
-  src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif"
-  alt="Map Background"
-/>
-
+        <img
+          className="h-full w-full object-cover"
+          src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif"
+          alt="Map Background"
+        />
       </div>
+         <div>
+          <h2>dd</h2>
+         </div>
+      <div
+        ref={locationPanelClose}
+        className="  flex flex-col justify-end bottom-0 absolute  w-full mb-0  "
+      >
+        <div onClick={()=>{setMapOpen(false)}} className="absolute left-42 bottom-20">
+          {!panelOpen?<img  className="w-10 rounded-full" src="/assets/up-arrow.png" alt="" />:''}
+           
+        </div>
 
-      <div ref={formClose} className="  flex flex-col justify-end top-0 h-screen absolute  w-full mb-0  ">
-        <div className="h-[40%] rounded-tl-2xl rounded-tr-xl p-5 bg-white relative">
+        <div
+          ref={formClose}
+          className="h-[40%] rounded-tl-2xl rounded-tr-xl p-5 bg-white relative"
+        >
           <h1 className="text-xl font-semibold mb-3">Find a trip</h1>
 
-          <img onClick={() =>
-            {  if (panelOpen) {
-              setPanelOpen(false);
-              setVehiclePanelOpen(false); 
-            }}} ref={arrowPanelClose} className="absolute top-6 right-6" src="/assets/arrow-down-s-line.png" alt="" />
+          <img
+            onClick={() => {
+              if (panelOpen) {
+                setPanelOpen(false);
+                setVehiclePanelOpen(false);
+              }
+            }}
+            ref={arrowPanelClose}
+            className="absolute top-6 right-6"
+            src="/assets/arrow-down-s-line.png"
+            alt=""
+          />
 
           <form onSubmit={(e) => submitHandler(e)} className="flex flex-col">
             <input
@@ -151,71 +253,106 @@ const UserHome = () => {
             </button>
           </form>
         </div>
-        <div ref={panelRef} className="bg-white h-0 mt-0 ">
-          <LocatioSearchPanel vehiclePanelOpen={vehiclePanelOpen} setVehiclePanelOpen={setVehiclePanelOpen}/>
+        <div ref={panelRef} className="bg-white h-0 mt-0  ">
+          <LocatioSearchPanel
+            vehiclePanelOpen={vehiclePanelOpen}
+            setVehiclePanelOpen={setVehiclePanelOpen}
+          />
         </div>
       </div>
 
-   <div ref={vehiclePanelRef} className="fixed  w-full bottom-0  bg-white px-3 py-6 z-10 flex flex-col gap-4 h-[60%]">
+      <div
+        ref={vehiclePanelRef}
+        className="fixed  w-full bottom-0  bg-white px-3 py-6 z-10 flex flex-col gap-4 h-[60%] "
+      >
         <h2 className="font-bold text-xl mb-3">Choose a Vehicle</h2>
-     <div className="flex active:border-2 rounded-xl flex-row gap-2">
-      <div> 
-        <img className="h-15 rounded-2xl" src="/assets/uber-car.jpeg" alt="" />
+        <div className="flex active:border-2 rounded-xl flex-row gap-2">
+          <div>
+            <img
+              className="h-15 rounded-2xl"
+              src="/assets/uber-car.jpeg"
+              alt=""
+            />
+          </div>
+          <div>
+            <div className="flex font-medium mt-1">
+              <h2>UberGo</h2>
+              <span>
+                <img
+                  className="w-5 mt-1"
+                  src="/assets/person-logo.png"
+                  alt=""
+                />
+              </span>
+            </div>
+            <div className="text-sm font-normal">
+              <h3 className="font-medium">2mins away</h3>
+              <h3>affordable, compact rides</h3>
+            </div>
+          </div>
+          <div className="items-center h-full ml-5 mt-6">
+            <h2>₹195</h2>
+          </div>
         </div>
-        <div>
-      <div className="flex font-medium mt-1">
-        <h2>UberGo</h2>
-        <span><img className="w-5 mt-1" src="/assets/person-logo.png" alt="" /></span>
-    </div>
-    <div className="text-sm font-normal">
-      <h3 className="font-medium">2mins away</h3>
-      <h3>affordable, compact rides</h3>
-    </div>
-    </div>
-    <div className="items-center h-full ml-5 mt-6">
-      <h2 >₹195</h2>
-    </div>
-    </div>
 
-     <div className="flex active:border-2 rounded-xl flex-row gap-6">
-      <div> 
-        <img className="h-15 rounded-2xl" src="/assets/uber-auto.jpeg" alt="" />
+        <div className="flex active:border-2 rounded-xl flex-row gap-6">
+          <div>
+            <img
+              className="h-15 rounded-2xl"
+              src="/assets/uber-auto.jpeg"
+              alt=""
+            />
+          </div>
+          <div>
+            <div className="flex font-medium mt-1">
+              <h2>UberAuto</h2>
+              <span>
+                <img
+                  className="w-5 mt-1"
+                  src="/assets/person-logo.png"
+                  alt=""
+                />
+              </span>
+            </div>
+            <div className="text-sm font-normal">
+              <h3 className="font-medium">2mins away</h3>
+              <h3>affordable, compact rides</h3>
+            </div>
+          </div>
+          <div className="items-center h-full ml-1 mt-6">
+            <h2>₹195</h2>
+          </div>
         </div>
-        <div>
-      <div className="flex font-medium mt-1">
-        <h2>UberAuto</h2>
-        <span><img className="w-5 mt-1" src="/assets/person-logo.png" alt="" /></span>
-    </div>
-    <div className="text-sm font-normal">
-      <h3 className="font-medium">2mins away</h3>
-      <h3>affordable, compact rides</h3>
-    </div>
-    </div>
-    <div className="items-center h-full ml-1 mt-6">
-      <h2 >₹195</h2>
-    </div>
-    </div>
 
-    <div className="flex active:border-2 rounded-xl flex-row gap-6">
-      <div> 
-        <img className="h-15 rounded-2xl" src="/assets/uber-bike.jpeg" alt="" />
+        <div className="flex active:border-2 rounded-xl flex-row gap-6">
+          <div>
+            <img
+              className="h-15 rounded-2xl"
+              src="/assets/uber-bike.jpeg"
+              alt=""
+            />
+          </div>
+          <div>
+            <div className="flex font-semibold mt-1">
+              <h2>Moto</h2>
+              <span>
+                <img
+                  className="w-5 mt-1"
+                  src="/assets/person-logo.png"
+                  alt=""
+                />
+              </span>
+            </div>
+            <div className="text-sm font-normal">
+              <h3 className="font-medium">2mins away</h3>
+              <h3>affordable, compact rides</h3>
+            </div>
+          </div>
+          <div className="items-center h-full ml-1 mt-6">
+            <h2>₹195</h2>
+          </div>
         </div>
-        <div>
-      <div className="flex font-semibold mt-1">
-        <h2>Moto</h2>
-        <span><img className="w-5 mt-1" src="/assets/person-logo.png" alt="" /></span>
-    </div>
-    <div className="text-sm font-normal">
-      <h3 className="font-medium">2mins away</h3>
-      <h3>affordable, compact rides</h3>
-    </div>
-    </div>
-    <div className="items-center h-full ml-1 mt-6">
-      <h2 >₹195</h2>
-    </div>
-    </div>
-     
- </div>
+      </div>
     </div>
   );
 };
