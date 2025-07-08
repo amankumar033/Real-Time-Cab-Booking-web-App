9fcde1ecda768a30b2eca7359f30524d82b815c5
amankumar033 <amankumar51462@gmail.com>
2025-07-08 16:10:46 +0530

    updated

--- Backend/db.js
+++ Backend/db.js
@@ -5,7 +5,3 @@ mongoose.connect(`${process.env.MONGODB_URL}/uber`)
 .then(function(){
     console.log("DB connected")
 })
-.catch(function(err){
-    console.log(err)
-})}
-module.exports=connectToDb;
